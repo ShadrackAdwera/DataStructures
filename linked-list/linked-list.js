981f1293c6cf106624efbfa56be056e4fa4bf365
FACTS  LIMITED <admin@factsafrica.com>
2020-08-23 21:46:25 +0300

    prepend to linked list

--- linked-list/linked-list.js
+++ linked-list/linked-list.js
@@ -2,40 +2,49 @@
 //Element should know about the next one in line
 
 class LinkedList {
-    constructor() {
-        this.head = null
-        this.tail = null
-    }
+  constructor() {
+    this.head = null;
+    this.tail = null;
+  }
 
-    append(value) {
-        const newNode = {value: value, next: null}
-        
-        if(this.tail) {
-            this.tail.next = newNode
-        }
-        this.tail = newNode
-        if(!this.head) {
-            this.head = newNode
-        }
-    }
+  append(value) {
+    const newNode = { value: value, next: null };
 
-    //Output all elements
-    toArray() {
-        const elements = []
-        let currNode = this.head
-
-        while(currNode) {
-            elements.push(currNode)
-            currNode = currNode.next
-        }
-        return elements
+    if (this.tail) {
+      this.tail.next = newNode;
     }
+    this.tail = newNode;
+    if (!this.head) {
+      this.head = newNode;
+    }
+  }
+
+  prepend(value) {
+    const newNode = { value: value, next: this.head };
+    this.head = newNode;
+    if (!this.tail) {
+      this.tail = newNode;
+    }
+  }
+
+  //Output all elements
+  toArray() {
+    const elements = [];
+    let currNode = this.head;
+
+    while (currNode) {
+      elements.push(currNode);
+      currNode = currNode.next;
+    }
+    return elements;
+  }
 }
 
-const linkedList = new LinkedList()
-linkedList.append(1)
-linkedList.append('Deez Nuts')
-linkedList.append(true)
-linkedList.append(1.2345)
+const linkedList = new LinkedList();
+linkedList.append(1);
+linkedList.append('Deez Nuts');
+linkedList.append(true);
+linkedList.append(1.2345);
+linkedList.prepend('First Value')
 
-console.log(linkedList.toArray())
+console.log(linkedList.toArray());
