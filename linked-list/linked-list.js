634044d7cb838321b21603628a6cd9fd72eb151e
FACTS  LIMITED <admin@factsafrica.com>
2020-09-08 06:30:01 +0300

    append method of the linked list

--- linked-list/linked-list.js
+++ linked-list/linked-list.js
@@ -3,40 +3,19 @@
 
 class LinkedList {
   constructor() {
-    this.head = null;
-    this.tail = null;
+    this.head = null
+    this.tail = null
   }
-
   append(value) {
-    const newNode = { value: value, next: null };
-
-    if (this.tail) {
-      this.tail.next = newNode;
+    const newNode = {value: value, next: null }
+    if(this.tail) {
+      this.tail.next = newNode
     }
-    this.tail = newNode;
-    if (!this.head) {
-      this.head = newNode;
-    }
-  }
+    this.tail = newNode
 
-  prepend(value) {
-    const newNode = { value: value, next: this.head };
-    this.head = newNode;
-    if (!this.tail) {
-      this.tail = newNode;
+    if(!this.head) {
+      this.head = newNode
     }
-  }
-
-  //Output all elements
-  toArray() {
-    const elements = [];
-    let currNode = this.head;
-
-    while (currNode) {
-      elements.push(currNode);
-      currNode = currNode.next;
-    }
-    return elements;
   }
 }
 
