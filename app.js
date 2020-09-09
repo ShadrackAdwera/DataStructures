754cfe2dbc8da90ab1c50301a164ecc46ff406c0
FACTS  LIMITED <admin@factsafrica.com>
2020-09-09 09:58:13 +0300

    solve a simple algorithm

---
+++ app.js
@@ -0,0 +1,31 @@
+//Find the first occurrence of a character in a String
+
+//SOLUTION 1 : Using loops
+
+const word = 'ribashongilogasheshiakili'
+
+const findRepeated = (str) => {
+    for(let i =0; i<str.length; i++) {
+        for(let j = i+1; j<=str.length; j++) {
+            if(str[i]===str[j]) {
+                return str[i]
+            }
+        }
+    }
+}
+
+//Time Complexity : O(n^2)
+
+//SOLUTION 2: Using Objects
+
+const repeatedChar = (str) => {
+    const strObj = {}
+    for(const char of str) {
+        if(strObj[char]) {
+            return char
+        }
+        strObj[char] = 1
+    }
+}
+
+console.log(repeatedChar(word))
