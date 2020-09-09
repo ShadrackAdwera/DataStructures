d1458bfc0d82aec2c6fde2f6ec3c4eecbd93d955
FACTS  LIMITED <admin@factsafrica.com>
2020-09-09 09:22:11 +0300

    create queue ds

---
+++ Queue/Queue.js
@@ -0,0 +1,27 @@
+class Queue {
+    constructor() {
+        this.items = []
+    }
+
+    enqueue(value) {
+        this.items.push(value)
+    }
+
+    dequeue() {
+        return this.items.shift()
+    }
+    toArray() {
+        return [...this.items]
+    }
+    isEmpty() {
+        return this.items.length === 0 
+    }
+}
+
+const queue = new Queue()
+queue.enqueue('a')
+queue.enqueue('b')
+queue.enqueue('c')
+console.log(queue.toArray())
+console.log('Dequeued ==> ', queue.dequeue())
+console.log(queue.toArray())
