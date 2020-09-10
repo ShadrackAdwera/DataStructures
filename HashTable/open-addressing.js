2307fa49751f71bc8b876b66543c874eff69b280
FACTS  LIMITED <admin@factsafrica.com>
2020-09-10 11:01:59 +0300

    use open addressing to solve hash collisions

--- HashTable/open-addressing.js
+++ HashTable/open-addressing.js
@@ -1,38 +1,39 @@
 class HashTable {
-    constructor() {
-        this.size = 1000
-        this.buckets = Array(1000).fill(null)
-    }
+  constructor() {
+    this.size = 1000;
+    this.buckets = Array(1000).fill(null);
+  }
 
-    hash(key) {
-        let hash = 0
-        for(const val of key) {
-            hash+=val.charCodeAt(0)
-        }
-        return hash % this.size
+  hash(key) {
+    let hash = 0;
+    for (const val of key) {
+      hash += val.charCodeAt(0);
     }
+    return hash % this.size;
+  }
 
-    set(key,value) {
-        let keyHash = this.hash(key)
-        if(this.buckets[keyHash]===null) {
-            this.buckets[keyHash] = value
-        } else {
-            while(this.buckets[keyHash] !==null) {
-                keyHash++
-            }
-        }
+  set(key, value) {
+    let keyHash = this.hash(key);
+    if (this.buckets[keyHash] === null) {
+      this.buckets[keyHash] = { key: key, value: value };
+    } else {
+      while (this.buckets[keyHash] !== null) {
+        keyHash++;
+      }
+      this.buckets[keyHash] = { key: key, value: value };
     }
+  }
 
-    get(key) {
-        const keyHash = this.hash(key)
-        return this.buckets[keyHash]
-    }
+  get(key) {
+    const keyHash = this.hash(key);
+    return this.buckets[keyHash];
+  }
 
-    showInfo() {
-        for(const key in this.buckets) {
-            if(this.buckets[key] !==null) {
-                return key + ' ==> ' + this.buckets[key]
-            }
-        }
+  showInfo() {
+    for (const key in this.buckets) {
+      if (this.buckets[key] !== null) {
+        return key + ' ==> ' + this.buckets[key];
+      }
     }
+  }
 }
